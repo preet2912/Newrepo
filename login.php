4bc2ca876c64a99c76673db46f8589dab0f392a0
unknown <navrakhra05@gmail.com>
2020-02-26 15:58:56 +0530

    auto push

---
+++ login.php
@@ -0,0 +1,184 @@
+<?php include 'inc/config.php'; ?>
+<?php include 'inc/template_start.php'; ?>
+
+<!-- Login Background -->
+<div id="login-background">
+    <!-- For best results use an image with a resolution of 2560x400 pixels (prefer a blurred image for smaller file size) -->
+    <!--<img src="img/placeholders/headers/login_header.jpg" alt="Login Background" class="animation-pulseSlow"> -->
+</div>
+<!-- END Login Background -->
+
+<!-- Login Container -->
+<div id="login-container" class="animation-fadeIn">
+    <!-- Login Title -->
+    <div class="login-title text-center">
+        <h1><i class="gi gi-lock"></i> <strong>Login</strong></h1>
+    </div>
+    <!-- END Login Title -->	
+
+    <!-- Login Block -->
+    <div class="block push-bit">
+		<div id="msg_box"></div>        
+	   <!-- Login Form -->
+        <form action="javascript(0);" method="post" id="loginForm" name="loginForm" class="form-horizontal form-bordered form-control-borderless">
+             <input type="hidden" name="action" id="action" value="login" />
+			<div class="form-group">
+                <div class="col-xs-12">
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="gi gi-envelope"></i></span>
+                        <input type="text" id="user_name" name="user_name" class="form-control input-lg" placeholder="User Name">
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-xs-12">
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="gi gi-asterisk"></i></span>
+                       <input type="password" id="password" name="password" class="form-control input-lg" placeholder="Password">
+                    </div>
+                </div>
+            </div>
+            <div class="form-group form-actions">
+				<div class="col-xs-12 text-center">
+                    <button type="button"  id="lg_btn" class="btn login_click_btn redbutton"> Login  </button>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-xs-12 text-center">
+                   <!--<a href="javascript:void(0)" id="link-reminder-login"><small>Forgot password?</small></a> -
+                    <a href="javascript:void(0)" id="link-register-login"><small>Create a new account</small></a>-->
+                </div>
+            </div> 
+        </form>
+        <!-- END Login Form -->
+
+        <!-- Reminder Form -->
+        <!--<form action="login.php#reminder" method="post" id="form-reminder" class="form-horizontal form-bordered form-control-borderless display-none">
+            <div class="form-group">
+                <div class="col-xs-12">
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="gi gi-envelope"></i></span>
+                        <input type="text" id="reminder-email" name="reminder-email" class="form-control input-lg" placeholder="Email">
+                    </div>
+                </div>
+            </div>
+            <div class="form-group form-actions">
+                <div class="col-xs-12 text-right">
+                    <button type="submit" class="btn btn-sm btn-primary"><i class="fa fa-angle-right"></i> Reset Password</button>
+                </div>
+            </div>
+           <div class="form-group">
+                <div class="col-xs-12 text-center">
+                    <small>Did you remember your password?</small> <a href="javascript:void(0)" id="link-reminder"><small>Login</small></a>
+                </div>
+            </div> 
+        </form> -->
+        <!-- END Reminder Form -->
+
+        <!-- Register Form -->
+       <!--<form action="login.php#register" method="post" id="form-register" class="form-horizontal form-bordered form-control-borderless display-none">
+            <div class="form-group">
+                <div class="col-xs-6">
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="gi gi-user"></i></span>
+                        <input type="text" id="register-firstname" name="register-firstname" class="form-control input-lg" placeholder="Firstname">
+                    </div>
+                </div>
+                <div class="col-xs-6">
+                    <input type="text" id="register-lastname" name="register-lastname" class="form-control input-lg" placeholder="Lastname">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-xs-12">
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="gi gi-envelope"></i></span>
+                        <input type="text" id="register-email" name="register-email" class="form-control input-lg" placeholder="Email">
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-xs-12">
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="gi gi-asterisk"></i></span>
+                        <input type="password" id="register-password" name="register-password" class="form-control input-lg" placeholder="Password">
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-xs-12">
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="gi gi-asterisk"></i></span>
+                        <input type="password" id="register-password-verify" name="register-password-verify" class="form-control input-lg" placeholder="Verify Password">
+                    </div>
+                </div>
+            </div>
+            <div class="form-group form-actions">
+                <div class="col-xs-6">
+                    <a href="#modal-terms" data-toggle="modal" class="register-terms">Terms</a>
+                    <label class="switch switch-primary" data-toggle="tooltip" title="Agree to the terms">
+                        <input type="checkbox" id="register-terms" name="register-terms">
+                        <span></span>
+                    </label>
+                </div>
+                <div class="col-xs-6 text-right">
+                    <button type="submit" class="btn btn-sm btn-success"><i class="fa fa-plus"></i> Register Account</button>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-xs-12 text-center">
+                    <small>Do you have an account?</small> <a href="javascript:void(0)" id="link-register"><small>Login</small></a>
+                </div>
+            </div>
+        </form> -->
+        <!-- END Register Form -->
+    </div>
+    <!-- END Login Block -->
+
+    <!-- Footer -->
+    <footer class="text-muted text-center">
+        <small><span>2019</span> &copy; <a href="http://mykioskmanager.com/" target="_blank"><?php echo $template['name'] . ' ' . $template['version']; ?></a></small>
+    </footer>
+    <!-- END Footer -->
+</div>
+<!-- END Login Container -->
+
+<!-- Modal Terms -->
+<div id="modal-terms" class="modal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title">Terms &amp; Conditions</h4>
+            </div>
+            <div class="modal-body">
+                <h4>Title</h4>
+                <p>Donec lacinia venenatis metus at bibendum? In hac habitasse platea dictumst. Proin ac nibh rutrum lectus rhoncus eleifend. Sed porttitor pretium venenatis. Suspendisse potenti. Aliquam quis ligula elit. Aliquam at orci ac neque semper dictum. Sed tincidunt scelerisque ligula, et facilisis nulla hendrerit non. Suspendisse potenti. Pellentesque non accumsan orci. Praesent at lacinia dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Donec lacinia venenatis metus at bibendum? In hac habitasse platea dictumst. Proin ac nibh rutrum lectus rhoncus eleifend. Sed porttitor pretium venenatis. Suspendisse potenti. Aliquam quis ligula elit. Aliquam at orci ac neque semper dictum. Sed tincidunt scelerisque ligula, et facilisis nulla hendrerit non. Suspendisse potenti. Pellentesque non accumsan orci. Praesent at lacinia dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <h4>Title</h4>
+                <p>Donec lacinia venenatis metus at bibendum? In hac habitasse platea dictumst. Proin ac nibh rutrum lectus rhoncus eleifend. Sed porttitor pretium venenatis. Suspendisse potenti. Aliquam quis ligula elit. Aliquam at orci ac neque semper dictum. Sed tincidunt scelerisque ligula, et facilisis nulla hendrerit non. Suspendisse potenti. Pellentesque non accumsan orci. Praesent at lacinia dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Donec lacinia venenatis metus at bibendum? In hac habitasse platea dictumst. Proin ac nibh rutrum lectus rhoncus eleifend. Sed porttitor pretium venenatis. Suspendisse potenti. Aliquam quis ligula elit. Aliquam at orci ac neque semper dictum. Sed tincidunt scelerisque ligula, et facilisis nulla hendrerit non. Suspendisse potenti. Pellentesque non accumsan orci. Praesent at lacinia dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <h4>Title</h4>
+                <p>Donec lacinia venenatis metus at bibendum? In hac habitasse platea dictumst. Proin ac nibh rutrum lectus rhoncus eleifend. Sed porttitor pretium venenatis. Suspendisse potenti. Aliquam quis ligula elit. Aliquam at orci ac neque semper dictum. Sed tincidunt scelerisque ligula, et facilisis nulla hendrerit non. Suspendisse potenti. Pellentesque non accumsan orci. Praesent at lacinia dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Donec lacinia venenatis metus at bibendum? In hac habitasse platea dictumst. Proin ac nibh rutrum lectus rhoncus eleifend. Sed porttitor pretium venenatis. Suspendisse potenti. Aliquam quis ligula elit. Aliquam at orci ac neque semper dictum. Sed tincidunt scelerisque ligula, et facilisis nulla hendrerit non. Suspendisse potenti. Pellentesque non accumsan orci. Praesent at lacinia dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+			<p>Donec lacinia venenatis metus at bibendum? In hac habitasse platea dictumst. Proin ac nibh rutrum lectus rhoncus eleifend. Sed porttitor pretium venenatis. Suspendisse potenti. Aliquam quis ligula elit. Aliquam at orci ac neque semper dictum. Sed tincidunt scelerisque ligula, et facilisis nulla hendrerit non. Suspendisse potenti. Pellentesque non accumsan orci. Praesent at lacinia dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+			
+			<p>Donec lacinia venenatis metus at bibendum? In hac habitasse platea dictumst. Proin ac nibh rutrum lectus rhoncus eleifend. Sed porttitor pretium venenatis. Suspendisse potenti. Aliquam quis ligula elit. Aliquam at orci ac neque semper dictum. Sed tincidunt scelerisque ligula, et facilisis nulla hendrerit non. Suspendisse potenti. Pellentesque non accumsan orci. Praesent at lacinia dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+			
+			
+			
+			
+        </div>
+    </div>
+</div>
+<!-- END Modal Terms -->
+
+<?php include 'inc/template_scripts.php'; ?>
+
+<!-- Load and execute javascript code used only in this page -->
+<script src="js/pages/login.js"></script>
+<!--<script>$(function(){ Login.init(); });</script> -->
+
+<?php include 'inc/template_end.php'; ?>
